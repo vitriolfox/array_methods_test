09c996c95793625509cf19aa3e6bc62cb0e15641
László Horváth <bedafoltyn@gmail.com>
2017-10-25 08:41:48 +0200

    array method exercise

---
+++ README.md
@@ -0,0 +1,40 @@
+# Gyakorló feladat: érettségi
+
+Az alábbi feladatban informatika érettségizők adatai alapján kell különböző feladatokat megoldani. Ahol lehet használj valamilyen tömb metódust.
+
+## Ajánlott tömb metódusok: 
+
+[map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+
+[filter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
+
+[reduce](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce) 
+
+[some](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some)
+
+[every](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every)
+
+## Érettségi ponthatárok:
+
+* 80-100  *Jeles*
+
+* 60-79  *Jó*
+
+* 40-59  *Közepes*
+
+* 25-39  *Elégséges*
+
+* 0-24  *Elégtelen*
+
+Az érettségizők életkora 18 év, de vannak *előrehozott érettségizők* is, akik fiatalabbak.
+
+
+## Tesztek
+
+A feladatokat akkor sikerült megoldani, ha minden teszt átmegy.
+
+A tesztek futtatásához először is telepiteni kell a függőségeket: **npm install**
+
+Ezután a tesztek a **npm test** paranccsal futtathatók.
+
+Összes *script* listázása: **npm run**
---
+++ index.js
@@ -0,0 +1,72 @@
+// LEVEL 1
+
+// tanulók névsora
+// return []
+const nameList = function (students) {};
+
+// tanulók átlagpontszáma
+// return Number
+const averageScore = function (students) {};
+// lány tanulók átlaga
+// return Number
+const averageFemaleScore = function (students) {};
+
+// átlag felett teljesitők listája
+// return []
+const aboveAverageStudents = function (students) {};
+
+// átment-e mindenki a teszten?
+// return Boolean
+const didEveryOnePass = function (students) {};
+
+// bukott diákok listája
+// return []
+const failedStudents = function (students) {};
+
+// hányan kaptak jeles értékelést?
+// return []
+const studentsWithA = function (students) {};
+
+// előrehozott érettségizők
+// return []
+const earlyGraduate = function (students) {};
+
+// legtöbb pontszámot elért tanuló
+// return {}
+const bestStudent = function (students) {};
+
+// legkevesebb pontszámot elért
+// return {}
+const worstStudent = function (students) {};
+
+// LEVEL 2
+
+// készits olyan függvényt amivel név alapján lehet tanulót kikeresni egy tömbből
+// csak egy tanulóval térjen vissza a függvény
+// return {}
+const findByName = function (students, name) {};
+
+// készits olyan függvényt amivel egy megadott pont intervallumban lévő diákokkal tér vissza
+// pl.: 25-46 között
+// return []
+const findInScoreRange = function (students, min, max) {};
+
+// készits olyan függvényt amivel nem szerint lekérheted a tanulók listáját
+// return []
+const findBySex = function (students, sex) {};
+
+module.exports = {
+  nameList,
+  averageScore,
+  averageFemaleScore,
+  aboveAverageStudents,
+  didEveryOnePass,
+  failedStudents,
+  studentsWithA,
+  earlyGraduate,
+  findByName,
+  findInScoreRange,
+  findBySex,
+  bestStudent,
+  worstStudent
+};
